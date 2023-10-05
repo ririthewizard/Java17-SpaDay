a728506788467e1c45e0332a90c3da4e2524e084
Riley <ririwilliams.rw@gmail.com>
2023-10-05 18:49:58 -0500

    Did some work

--- src/main/java/org/launchcode/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/controllers/SpaDayController.java
@@ -63,6 +63,12 @@ public class SpaDayController {
             }
         }
 
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("facials", facials);
+        model.addAttribute("appropriateFacials", appropriateFacials);
+        model.addAttribute("manipedi", manipedi);
+
         return "menu";
     }
 }
